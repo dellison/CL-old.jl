6a7a6cd99246f27464eb5a58eb9827bf0a170f50
David Ellison <davidehellison@gmail.com>
2017-08-13 21:43:41 -0700

    add tests for bigram HMM tagger

--- test/test_markov.jl
+++ test/test_markov.jl
@@ -1,5 +1,9 @@
+import CL: MarkovModel, MarkovState, HMM
+
+import CL: p_state_cond, p_observ_state, viterbi
+
 function test_markov_state()
-    state = CL.MarkovState()
+    state = MarkovState()
     inc!(state, "a")
     inc!(state, "b")
     inc!(state, "b")
@@ -13,5 +17,50 @@ function test_markov_models()
     train!(m, ["hello"])
 end
 
+function test_bigram_hmm()
+    m = HMM(1) # first-order markov model
+
+    corpus = ["the/DT cat/NN ate/VBD ./.",
+              "the/DT dogs/NNS ate/VBD food/NN ./.",
+              "cats/NNS ate/VBD ./.",
+              "cop/NN dogs/VBZ suspect/NN"]
+    for sent in corpus
+        train!(m, map(tkn->split(tkn,"/"), split(sent)))
+    end
+
+    bos_history = [m.state_model.start_sym]
+
+    @test p_state_cond(m, bos_history, "DT") == 1/2
+    @test p_state_cond(m, bos_history, "NN") == 1/4
+    @test p_state_cond(m, bos_history, "NNS") == 1/4
+
+    @test p_state_cond(m, bos_history, "DT") == 1/2
+    @test p_state_cond(m, ["DT"], "NNS") == 1/2
+    @test p_state_cond(m, ["NNS"], "VBD") == 1.0
+    @test p_state_cond(m, ["VBD"], ".") == 2/3
+
+    @test p_observ_state(m, "the", "DT") == 1.0
+    @test p_observ_state(m, "dogs", "VBZ") == 1.0
+    @test p_observ_state(m, "dogs", "NNS") == 0.5
+
+    states, lprob = CL.viterbi(m, split("the dogs ate ."))
+
+    p_the_dt = 1.0 * 0.5     # P(the|DT) * P(DT|<BOS>)
+    p_dogs_nns = 0.5 * (1/2) # P(dogs|NNS) * P(NNS|DT)
+    p_ate_vbd = 1.0 * 1.0    # P(ate|VBD) * P(VBD|NNS)
+    p_period = 1.0 * (2/3)   # P(.|.) * P(.|VBD)
+
+    sent_prob = log(p_the_dt) + log(p_dogs_nns) + log(p_ate_vbd) + log(p_period)
+    @test sent_prob == lprob
+end
+
+function test_trigram_hmm()
+    m = HMM(2)
+    corpus = []
+    bos_history = [m.state_model.start_sym,m.state_model.start_sym]
+end
+
 test_markov_state()
 test_markov_models()
+test_bigram_hmm()
+test_trigram_hmm()
