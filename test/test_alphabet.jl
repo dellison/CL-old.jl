fc143003c1d7b55f057bebd06fd4c2a9be2ce88e
David Ellison <davidehellison@gmail.com>
2017-08-28 20:35:52 -0700

    more alphabet tests

--- test/test_alphabet.jl
+++ test/test_alphabet.jl
@@ -1,4 +1,4 @@
-import CL: Alphabet
+import CL: Alphabet, lookup, plaintext
 
 function test_alphabet()
     a = Alphabet()
@@ -10,6 +10,9 @@ function test_alphabet()
     @test "lol" in a
     @test "lol2" in a
 
+    @test lookup(a, 1) == "lol"
+    @test lookup(a, 2) == "lol2"
+
     a = Alphabet(split("one two three"))
     @test a["one"] == 1
     @test a["two"] == 2
@@ -18,6 +21,8 @@ function test_alphabet()
     @test !("four" in a)
     @test a["four"] == 4
     @test "four" in a
+
+    @test plaintext(a) == "one\ntwo\nthree\nfour"
 end
 
 test_alphabet()
